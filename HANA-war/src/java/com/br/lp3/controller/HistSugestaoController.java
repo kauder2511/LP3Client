c3269964c5441727a817a9db13ff4b73a99a80bf
RaqG <raqbgallo@gmail.com>
2015-11-26 21:57:10 -0200

    Modificado telas

--- HANA-war/src/java/com/br/lp3/controller/HistSugestaoController.java
+++ HANA-war/src/java/com/br/lp3/controller/HistSugestaoController.java
@@ -1,7 +1,17 @@
 package com.br.lp3.controller;
 
+import com.br.lp3.DAO.DAOHistSugestaoLocal;
+import com.br.lp3.DAO.DAOHistoriaLocal;
+import com.br.lp3.entities.Heroi;
+import com.br.lp3.entities.Historia;
+import com.br.lp3.entities.Histsugestao;
+import com.br.lp3.entities.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +22,11 @@ import javax.servlet.http.HttpServletResponse;
  * @author racme
  */
 public class HistSugestaoController extends HttpServlet {
+
+    @EJB
+    private DAOHistoriaLocal dAOHistoria;
+    @EJB
+    private DAOHistSugestaoLocal dAOHistSugestao;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -26,16 +41,45 @@ public class HistSugestaoController extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HistSugestaoController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HistSugestaoController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            Heroi heroi = (Heroi) request.getSession().getAttribute("heroiHistSelecionado");
+            Usuario userDaSugestao = (Usuario) request.getSession().getAttribute("Usuario");
+            String introducao = request.getAttribute("editIntro").toString();
+            String meio = request.getAttribute("editMeio").toString();
+            String conclusao = request.getAttribute("editFim").toString();
+
+            for (Historia his : dAOHistoria.readList()) {
+                if (Objects.equals(his.getIdheroi().getIdHeroi(), heroi.getIdHeroi())) {
+                    if (his.getRoteiro() == 1) {
+                        if (!his.getHistoria().equals(introducao)) {
+                            Histsugestao sugIntro = new Histsugestao();
+                            sugIntro.setHistoria(introducao);
+                            sugIntro.setIdheroi(heroi);
+                            sugIntro.setIdusuario(userDaSugestao);
+                            sugIntro.setRoteiro(1);
+                            dAOHistSugestao.inserir(sugIntro);
+                        }
+                    } else if (his.getRoteiro() == 2) {
+                        if (!his.getHistoria().equals(meio)) {
+                            Histsugestao sugMeio = new Histsugestao();
+                            sugMeio.setHistoria(meio);
+                            sugMeio.setIdheroi(heroi);
+                            sugMeio.setIdusuario(userDaSugestao);
+                            sugMeio.setRoteiro(2);
+                            dAOHistSugestao.inserir(sugMeio);
+                        }
+                    } else if (his.getRoteiro() == 3) {
+                        if (!his.getHistoria().equals(conclusao)) {
+                            Histsugestao sugConclusao = new Histsugestao();
+                            sugConclusao.setHistoria(conclusao);
+                            sugConclusao.setIdheroi(heroi);
+                            sugConclusao.setIdusuario(userDaSugestao);
+                            sugConclusao.setRoteiro(3);
+                            dAOHistSugestao.inserir(sugConclusao);
+                        }
+                    }
+                }
+            }
+            response.sendRedirect("TelaSugestao.jsp");
         }
     }
 
